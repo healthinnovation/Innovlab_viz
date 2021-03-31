0208e9a491b0e5f7fbf0f4b33a89f48237290952
ambarja <antony.barja8@gmail.com>
2021-03-30 22:34:11 -0500

    add gha

--- 01_dowloand_data.R
+++ 01_dowloand_data.R
@@ -2,11 +2,11 @@ library(tidyverse)
 library(vroom)
 library(RCurl)
 
-db <- vroom(getURL("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"))
+db <- vroom(getURL("https://covid.ourworldindata.org/data/owid-covid-data.csv"))
 
 
 saveRDS(db,
-  file = "./vacuna_mundial.rds"
+  file = "./resources/vacuna_mundial.rds"
 )
 
 
